a06f4a0d07293f40e1bdf9f1cbb1af54da3ff7cb
José Antonio León López <joseantonioleonlopez206@gmail.com>
2024-04-01 10:21:03 +0200

    sidebar completado

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -13,28 +13,28 @@ const Sidebar = () => {
       <ul className="list-unstyled components">
         <li>
           <Link to="/">
-            <i className="fas fa-home"></i> Publicaciones
+            <i className="fas fa-home"></i> <span>Publicaciones</span>
           </Link>
         </li>
         <li>
           <Link to="/user">
-            <i className="fas fa-user"></i> Perfil
+            <i className="fas fa-user"></i> <span>Perfil</span>
           </Link>
         </li>
         <li>
           <Link to="/users">
-            <i className="fas fa-users"></i> Explorar
+            <i className="fas fa-users"></i> <span>Explorar</span>
           </Link>
         </li>
         <li>
           <Link to="/topPublications">
-            <i className="fas fa-star"></i> Top Publicaciones
+            <i className="fas fa-star"></i> <span>Top Publicaciones</span>
           </Link>
         </li>
       </ul>
       <div className="logout">
         <Link to="/logout">
-          <i className="fas fa-sign-out-alt"></i> Cerrar Sesión
+          <i className="fas fa-sign-out-alt"></i> <span>Cerrar Sesión</span>
         </Link>
       </div>
     </nav>
