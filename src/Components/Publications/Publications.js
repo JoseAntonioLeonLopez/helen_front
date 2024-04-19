63d018a7b0cd4eb5c847c587a071ccb9abd8cc97
Jose Antonio Leon Lopez <joseantonioleonlopez206@gmail.com>
2024-04-19 09:03:45 +0200

    Pantalla publications mejorada

--- src/Components/Publications/Publications.js
+++ src/Components/Publications/Publications.js
@@ -6,6 +6,7 @@ import './Publications.css';
 
 const Publication = () => {
   const [publications, setPublications] = useState([]);
+  const [users, setUsers] = useState([]);
   const [loaded, setLoaded] = useState(false); // Estado local para controlar si las publicaciones se han cargado
 
   useEffect(() => {
@@ -33,53 +34,92 @@ const Publication = () => {
     fetchData();
   }, []);
 
-  const handleDelete = async (id) => {
-    try {
-      const token = localStorage.getItem('token');
-      await axios.delete(`${API_URL}/publications/${id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      // Eliminar la publicación localmente
-      setPublications(publications.filter(pub => pub.idPublication !== id));
-      // Mostrar mensaje de éxito
-      Swal.fire({
-        icon: 'success',
-        title: 'Éxito',
-        text: 'La publicación ha sido eliminada exitosamente.'
-      });
-    } catch (error) {
-      console.error('Error deleting publication:', error);
-      // Mostrar mensaje de error
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Ocurrió un error al eliminar la publicación. Por favor, inténtalo de nuevo más tarde.'
-      });
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const userPromises = publications.map(async (publication) => {
+          const response = await axios.get(`${API_URL}/users/${publication.fkUser}`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          return response.data;
+        });
+        const users = await Promise.all(userPromises);
+        setUsers(users);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Ocurrió un error al cargar la información de los usuarios. Por favor, inténtalo de nuevo más tarde.'
+        });
+      }
+    };
+
+    if (publications.length > 0) {
+      fetchUsers();
     }
+  }, [publications]);
+
+  const renderPublicationCards = () => {
+    return publications.map((publication, index) => (
+      <div key={publication.idPublication} className="col-md-4 mb-4">
+        <div className="card">
+          <img src={publication.image} className="card-img-top" alt={publication.title} />
+          <div className="card-body">
+            <p className='card-text'><small className='text-muted'>{publication.city}</small></p>
+            <h5 className="card-title">{publication.title}</h5>
+            <p className="card-text">{publication.description}</p>
+            {loaded && users[index] && (
+              <>
+                <div className="card-text">
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    {users[index].imageUser ? (
+                      <img
+                        src={users[index].imageUser}
+                        alt="Imagen de perfil"
+                        style={{
+                          background: 'white',
+                          width: '150px',
+                          height: '150px',
+                          borderRadius: '50%',
+                          objectFit: 'cover',
+                        }}
+                      />
+                    ) : (
+                      <img
+                        src="/img/user-avatar.svg"
+                        alt="Imagen de perfil predeterminada"
+                        style={{
+                          background: 'white',
+                          width: '30px',
+                          height: '30px',
+                          borderRadius: '50%',
+                          objectFit: 'cover',
+                        }}
+                      />
+                    )}
+                    <span style={{ marginLeft: '10px' }}>{users[index].username}</span>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    ));
   };
 
   return (
     <div className="container">
       <div className="row justify-content-center">
         {/* Verificar si las publicaciones se han cargado y si hay publicaciones */}
-        {loaded && Array.isArray(publications) && publications.length > 0 ? (
-          publications.map((publication) => (
-            <div key={publication.idPublication} className="col-md-4 mb-4">
-              <div className="card">
-                <img src={publication.image} className="card-img-top" alt={publication.title} />
-                <div className="card-body">
-                  <p className='card-text'><small className='text-muted'>{publication.city}</small></p>
-                  <h5 className="card-title">{publication.title}</h5>
-                  <p className="card-text">{publication.description}</p>
-                  <button onClick={() => handleDelete(publication.idPublication)} className="btn btn-danger">Eliminar</button>
-                </div>
-              </div>
-            </div>
-          ))
+        {loaded && publications.length > 0 ? (
+          renderPublicationCards()
         ) : (
-          // Mostrar mensaje si no hay publicaciones, si no se han cargado o si ocurrió un error al cargar
+          // Mostrar mensaje si no hay publicaciones o si no se han cargado
           <div className="col-md-12 text-center">
             <p>{loaded ? 'No hay publicaciones disponibles' : 'Cargando publicaciones...'}</p>
           </div>
